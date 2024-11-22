8238f4e09f7971c98c62258c5b49d56e3d19a3bf
byarthurm <167575443+pikaroto69@users.noreply.github.com>
2024-11-22 08:48:44 -0300

    mudanças e responsivo

--- static/scriptFin.js
+++ static/scriptFin.js
@@ -1,10 +1,12 @@
 const btnTransacao = document.getElementById("novaTransacaoBtn")
-const descInput = document.getElementById("descricao")
-const precoInput = document.getElementById("preco")
-const listaTransacoes = document.getElementById("listaTransacoes").getElementsByTagName('tbody')[0]
-const data = new Date().toLocaleDateString("pt-BR")
 const btnEntrada = document.querySelector(".income")
 const btnSaida = document.querySelector(".outflow")
+
+const descInput = document.getElementById("descricao")
+const precoInput = document.getElementById("preco")
+
+const listaTransacoes = document.getElementById("listaTransacoes").getElementsByTagName('tbody')[0]
+const data = new Date().toLocaleDateString("pt-BR")
 
 const saldoTotalCard = document.getElementById("saldoTotal")
 const saidasCard = document.getElementById("totalSaidas")
@@ -16,14 +18,7 @@ const closeBtn = document.getElementById('closeModal')
 const modalEdit = document.getElementById('modalEditaTransacao')
 const closeEdit = document.getElementById('closeModalEdita')
 
-closeEdit.addEventListener('click', () => {
-    modalEdit.style.display = 'none'  
-})
 
-
-var saldoTotal = 0
-var saida = 0
-var entrada = 0
 
 function abrirModalEditar(id, desc, preco) {
     document.getElementById("id").value = id
@@ -35,6 +30,10 @@ function abrirModalEditar(id, desc, preco) {
     modalEdit.style.display = 'flex'
 }
 
+closeEdit.addEventListener('click', () => {
+    modalEdit.style.display = 'none'  
+})
+
 
 function atualizarSaldo() {
     saldoTotalCard.textContent = `R$ ${saldoTotal.toFixed(2).replace(".", ",")}`
@@ -44,48 +43,12 @@ function atualizarSaldo() {
 
 
 const adicionarTransacao = (tipoTransacao) => {
-    const descValue = descInput.value
-    const precoValue = parseFloat(precoInput.value)
 
     if (!descValue || isNaN(precoValue) || precoValue <= 0 || tipoTransacao === null) {
         alert("Preencha todos os campos corretamente e defina o tipo de transação!")
         return
     }
 
-    const novaLinha = document.createElement("tr")
-    if (tipoTransacao === "entrada"){
-        novaLinha.innerHTML = `
-        <td>${descValue}</td>            
-        <td class="entrada" style="color:#007C59">R$ ${precoValue.toFixed(2).replace(".", ",")}</td>
-        <td style="color:#007C59">Entrada</td> 
-        <td>${data}</td>
-    `
-    }
-    else{
-        novaLinha.innerHTML = `
-            <td>${descValue}</td>            
-            <td class="saida" style="color:#8D2C2C">R$ ${precoValue.toFixed(2).replace(".", ",")}</td>
-            <td style="color:#8D2C2C">Saída</td>    
-            <td>${data}</td>
-    `
-    }
-
-    listaTransacoes.appendChild(novaLinha)
-
-    if (tipoTransacao === "entrada") {
-        saldoTotal += precoValue
-        entrada += precoValue
-    } else if (tipoTransacao === "saida") {
-        saldoTotal -= precoValue
-        saida += precoValue
-    }
-
-    atualizarSaldo()
-
-    descInput.value = ""
-    precoInput.value = ""
-    tipoTransacao = null
-    modal.style.display = 'none'
 }
 
 btnTransacao.addEventListener('click', () => {
@@ -94,11 +57,13 @@ btnTransacao.addEventListener('click', () => {
 
 
 closeBtn.addEventListener('click', () => {
-    modal.style.display = 'none'  
+    modal.style.display = 'none'
+ 
+    descInput.value = ""
+    precoInput.value = ""
+    tipoTransacao = null
+    modal.style.display = 'none' 
 })
-
-
-
 
 
 const saveButton = document.getElementById("save")
@@ -110,8 +75,5 @@ saveButton.addEventListener("click", () => {
     } else {
         alert("Por favor, selecione o tipo de transação.")
     }
-
-
     modal.style.display = 'none' 
 })
-
